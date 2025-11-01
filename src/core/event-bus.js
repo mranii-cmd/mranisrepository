0cd055a11f241c37933ef1296aebda7b6c867ef2
mranii-cmd <mrani.i@ucd.ac.ma>
2025-11-01 14:19:57 +0100

    Add EventBus class for publish/subscribe pattern

---
+++ src/core/event-bus.js
@@ -0,0 +1,22 @@
+class EventBus {
+    constructor() {
+        this.listeners = {};
+    }
+
+    subscribe(event, fn) {
+        if (!this.listeners[event]) {
+            this.listeners[event] = [];
+        }
+        this.listeners[event].push(fn);
+    }
+
+    unsubscribe(event, fn) {
+        if (!this.listeners[event]) return;
+        this.listeners[event] = this.listeners[event].filter(listener => listener !== fn);
+    }
+
+    publish(event, data) {
+        if (!this.listeners[event]) return;
+        this.listeners[event].forEach(listener => listener(data));
+    }
+}
